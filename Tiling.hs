fa1c351518fe19b36e2a09559bc3c9587894d422
Michael Fulton <fultonms@clarkson.edu>
2017-02-06 18:25:28 -0500

    Titling work

--- Tiling.hs
+++ Tiling.hs
@@ -4,22 +4,21 @@ type Tile = (Int, Int, Int, Int)
 nullTile :: Tile
 nullTile = (0,0,0,0)
 
-tiles = [(1,3,2,4), (1,3,4,2), (3,1,2,4), (3,1,4,2), (1,2,4,3)]
-
-graph = [[(1,4,2,4), (1,4,4,3), (1,4,3,2)],[(4,1,2,2), (4,1,2,2), (4,1,2,2)],[(1,3,4,2), (1,4,2,4), (1,4,4,3)]]
+tiles :: [Tile]
+tiles = [(1,3,4,2), (3,3,2,4), (1,4,3,2), (4,1,2,2), (1,1,3,4)]
 
 --A Grid is composed of a list of list of tiles.  The inner lists are rows.
 type Grid = [[Tile]]
 
-bounds :: (Int, Int)
-bounds = (2,2)
+size :: Int
+size = 3
 
 type State = Grid
 
 initial :: State
-initial =  [[(0,0,0,0), (0,0,0,0), (1,2,3,4)], 
-            [(0,0,0,0), (0,0,0,0), (1,2,3,4)], 
-            [(1,2,3,4), (1,2,3,4), (1,2,3,4)]]
+initial =  [[(1,4,3,2)], 
+            [(4,1,2,2)], 
+            [(1,3,4,2), (1,4,2,4), (1,4,4,3)]]
 
 loser :: State
 loser = []
@@ -29,7 +28,7 @@ testRow [] = True
 testRow [x] = True
 testRow (x:xs) = let (_,_,_, r) = x
                      (_,_,l,_ ) = (head xs) 
-                 in ((r == l) || ((r == 0) || (l == 0))) && testRow(xs)
+                 in (r == l) && testRow(xs)
 
 testRows :: [[Tile]] -> Bool
 testRows [] = True
@@ -40,11 +39,26 @@ testCol [] = True
 testCol [x] = True
 testCol (x:xs) = let (_,b,_,_) = x
                      (t,_,_,_ ) = (head xs) 
-                 in ((t == b) || ((t == 0) || (b == 0))) && testCol(xs)
+                 in (t == b) && testCol(xs)
 
 testCols :: [[Tile]] -> Bool
 testCols [] = True
 testCols (x:xs) = testCol(x) && testCols(xs)
 
+--transformToCol :: [[Tile]] -> [[Tile]]
+--transformToCol [] = []
+--transformToCol g  = [
+
+rowsComplete :: [Int] -> Bool
+rowsComplete [] = True
+rowsComplete (x:xs) = (x == size) && rowsComplete xs
+
 goalTest :: State -> Bool
-goalTest s = testRows(s) && (testCols [[x | x <- map (!! col) s ] | col <- [0..fst bounds]])
+goalTest s = testRows s && testCols (transformToCol (map reverse s))  && rowsComplete (map length s)
+
+--genNext :: [Tile] -> State -> [State]
+--genNext [] s = []
+--genNext (t:ts) s = [x | x <- s ] ++ (genNext ts s)
+
+--operator :: State -> [State]
+--operator s = [x | x <- genNext tiles s]
