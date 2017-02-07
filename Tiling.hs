86934776b2fe3df9dd339a8a8c5d1520babb2807
Michael Fulton <fultonms@clarkson.edu>
2017-02-06 20:46:12 -0500

    Tiling done for now.

--- Tiling.hs
+++ Tiling.hs
@@ -16,18 +16,18 @@ size = 3
 type State = Grid
 
 initial :: State
-initial =  [[(1,4,3,2)], 
-            [(4,1,2,2)], 
+initial =  [[                      (1,4,3,2)],
+            [                      (4,1,2,2)],
             [(1,3,4,2), (1,4,2,4), (1,4,4,3)]]
 
 loser :: State
-loser = []
+loser = [[]]
 
 testRow :: [Tile] -> Bool
 testRow [] = True
 testRow [x] = True
 testRow (x:xs) = let (_,_,_, r) = x
-                     (_,_,l,_ ) = (head xs) 
+                     (_,_,l,_ ) = (head xs)
                  in (r == l) && testRow(xs)
 
 testRows :: [[Tile]] -> Bool
@@ -38,27 +38,49 @@ testCol :: [Tile] -> Bool
 testCol [] = True
 testCol [x] = True
 testCol (x:xs) = let (_,b,_,_) = x
-                     (t,_,_,_ ) = (head xs) 
-                 in (t == b) && testCol(xs)
+                     (t,_,_,_ ) = (head xs)
+                 in (t == b) && testCol(xs) && (t /= 0 && b /= 0)
 
 testCols :: [[Tile]] -> Bool
 testCols [] = True
 testCols (x:xs) = testCol(x) && testCols(xs)
 
---transformToCol :: [[Tile]] -> [[Tile]]
---transformToCol [] = []
---transformToCol g  = [
+getColumn :: Int -> [Tile] -> Tile
+getColumn col [] = (0,0,0,0)
+getColumn col (x:xs)
+  | col == 0  = x
+  |otherwise  = getColumn (col -1) xs
+
+
+transformToCol :: [[Tile]] -> [[Tile]]
+transformToCol graph = [[ x | x <- (map (getColumn col) (map reverse graph))]| col <- [0..size-1]]
 
 rowsComplete :: [Int] -> Bool
 rowsComplete [] = True
 rowsComplete (x:xs) = (x == size) && rowsComplete xs
 
 goalTest :: State -> Bool
-goalTest s = testRows s && testCols (transformToCol (map reverse s))  && rowsComplete (map length s)
+goalTest s = testRows s && testCols (transformToCol s)  && rowsComplete (map length s)
 
---genNext :: [Tile] -> State -> [State]
---genNext [] s = []
---genNext (t:ts) s = [x | x <- s ] ++ (genNext ts s)
+genState :: Tile -> Int -> State -> State
+genState t offset graph
+  | offset == size                    = []
+  | (length (graph !! offset)) < size = [([t] ++ graph!!offset)] ++ genState t (offset + 1) graph
+  | otherwise                         = [(graph!!offset)] ++ genState t (offset + 1) graph
 
---operator :: State -> [State]
---operator s = [x | x <- genNext tiles s]
+genNextStates :: [Tile] -> State -> [State]
+genNextStates [] s = []
+genNextStates (t:ts) s = [[x | x <- genState t 0 (reverse s)]] ++ (genNextStates ts s)
+
+operator :: State -> [State]
+operator s = [x | x <- genNextStates tiles s]
+
+search :: State
+search = treeSearch [initial] operator
+
+treeSearch :: [State] -> (State -> [State]) -> State
+treeSearch [] op = loser
+treeSearch (x:xs) op
+ | goalTest x  = x
+ | otherwise   = treeSearch (newstates ++ xs) op
+  where newstates = op x
